6c6ec297c990acfe8bdf2cd1b0427e7af142c6d3
JensC <jenscollaertprive@hotmail.com>
2022-03-25 23:41:05 +0100

    brr

--- src/components/SimpleForm.vue
+++ src/components/SimpleForm.vue
@@ -6,15 +6,12 @@
 				<div>
 					<input id="formname" type="text" v-model="form.formName" placeholder="Form Name">
 				</div>
-
 				<div>
 					<input id="formtitle" type="text" v-model="form.formtitle" placeholder="Form Title">
 				</div>
-
 				<div>
 					<input id="formcontent" type="text" v-model="form.formcontent" placeholder="Form Content">
 				</div>
-
 			</div>
             <div class="form-buttons">
                 <div class="button-row" v-for="(button, index) in formButtons" :key="index">
@@ -24,28 +21,23 @@
                             @click="deleteButton( index )">
                             &times;
                         </span>
-
 						<div>
 							<input id="buttontext" v-model="button.text" :name="`formButtons[${index}][text]`" type="text" class="form-control" placeholder="Button Text">
 						</div>
-
 						<div>
 							<input id="buttonurl" v-model="button.image" :name="`formButtons[${index}][image]`" type="text" class="form-control" placeholder="Button Image URL">
 						</div>
-
 						<div>
-								<select id="buttonactiontype" v-model="button.actiontype">
-									<option value="" disabled selected hidden>Select actiontype</option>
-									<option value="server">server</option>
-									<option value="command">command</option>
-									<option value="form">form</option>
-								</select>
+							<select id="buttonactiontype" v-model="button.actiontype">
+								<option value="" disabled selected hidden>Select actiontype</option>
+								<option value="server">server</option>
+								<option value="command">command</option>
+								<option value="form">form</option>
+							</select>
 						</div>	
-
 						<div>
 							<input id="buttonaction" v-model="button.action" :name="`formButtons[${index}][action]`" type="text" class="form-control" placeholder="Button Action">
 						</div>
-
                     </div>
                 </div>
             </div>
@@ -94,11 +86,9 @@ export default {
 		createConfig () {
 
 			var employees = [this.form, this.formButtons]
-
 			const doc = new YAML.Document()
 			doc.contents = employees
 			console.log(doc.toString())
-
 		}
     }
 }
